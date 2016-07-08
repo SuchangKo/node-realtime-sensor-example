4c3994457b499fc40789b926913fb30903531386
Ubuntu <drimaes@DrimAES.d3qohqeaytce1nwi0wpmgt1sgh.mx.internal.cloudapp.net>
2016-07-08 07:26:32 +0000

    Add SMulti-User (Socket) and POST param

--- realtimeServer.js
+++ realtimeServer.js
@@ -1,8 +1,29 @@
 //TODO: read the port from heroku
 
 var http = require('http'), 
-    fs = require('fs');
+    fs = require('fs'),
+    qs = require('querystring');
+var mongo = require('mongodb');
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
 
+var server = new Server('localhost',27017,{auto_reconnect:true});
+db = new Db('sensordb',server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'sensordb' database");
+        db.collection('sensordb', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'sensordb' collection doesn't exist. Creating it with sample data...");
+                //populateDB();
+            }
+        });
+    }
+});
+
+    
 var currentTemperature;
 
 var port = process.env.PORT || 1337;
@@ -14,16 +35,37 @@ var server = http.createServer(function (req, res) {
         if(req.method === 'POST'){
             var data = '';
             req.on('data',function(s){
-                data += s; 
+                //console.log("log : "+s);
+		data += s; 
             });
             req.on('end',function(){
-                currentTemperature = JSON.parse(data);
-
+		var postdata = qs.parse(data);
+		//console.log("data : "+postdata['val']);		
+		//currentTemperature = JSON.parse(data);
+		//console.log(jsonObj.val);
+		currentTemperature = postdata['val'];
+		currentTemperature *= 1; 
                 console.log('updated temperature to',currentTemperature);
+		
+		
+var sensordata = {
+	value : currentTemperature
+}
+
+db.collection('sensordb', function(err, collection) {
+    collection.insert(sensordata, {safe:true}, function(err, result) {
+        if (err) {
+            //res.send({'error':'An error has occurred'});
+        } else {
+            console.log('Success: ' + JSON.stringify(result[0]));
+            //res.send(result[0]);
+		}
+	});
+});
 
                 res.writeHead(200, {'Content-Type': 'application/json'});
                 res.end(JSON.stringify(currentTemperature));
-
+		
                 //send out to websocket
                 if(websocket){
                     websocket.emit('sensor', currentTemperature);
